df773c6ce804da15063e7b36fe55356ba082a6b9
AG <avnish1433@gmail.com>
2020-06-29 15:37:22 +0530

    Concurrent Traffic Simulation Project Udacity

---
+++ build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/avn187/Desktop/Addverb_Avnish/C++/Concurency/Concurrent-Traffic-Simulation-cpp/src/Graphics.cpp" "/home/avn187/Desktop/Addverb_Avnish/C++/Concurency/Concurrent-Traffic-Simulation-cpp/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/home/avn187/Desktop/Addverb_Avnish/C++/Concurency/Concurrent-Traffic-Simulation-cpp/src/Intersection.cpp" "/home/avn187/Desktop/Addverb_Avnish/C++/Concurency/Concurrent-Traffic-Simulation-cpp/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/home/avn187/Desktop/Addverb_Avnish/C++/Concurency/Concurrent-Traffic-Simulation-cpp/src/Street.cpp" "/home/avn187/Desktop/Addverb_Avnish/C++/Concurency/Concurrent-Traffic-Simulation-cpp/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/home/avn187/Desktop/Addverb_Avnish/C++/Concurency/Concurrent-Traffic-Simulation-cpp/src/TrafficLight.cpp" "/home/avn187/Desktop/Addverb_Avnish/C++/Concurency/Concurrent-Traffic-Simulation-cpp/build/CMakeFiles/traffic_simulation.dir/src/TrafficLight.cpp.o"
+  "/home/avn187/Desktop/Addverb_Avnish/C++/Concurency/Concurrent-Traffic-Simulation-cpp/src/TrafficObject.cpp" "/home/avn187/Desktop/Addverb_Avnish/C++/Concurency/Concurrent-Traffic-Simulation-cpp/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/home/avn187/Desktop/Addverb_Avnish/C++/Concurency/Concurrent-Traffic-Simulation-cpp/src/TrafficSimulator-Final.cpp" "/home/avn187/Desktop/Addverb_Avnish/C++/Concurency/Concurrent-Traffic-Simulation-cpp/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-Final.cpp.o"
+  "/home/avn187/Desktop/Addverb_Avnish/C++/Concurency/Concurrent-Traffic-Simulation-cpp/src/Vehicle.cpp" "/home/avn187/Desktop/Addverb_Avnish/C++/Concurency/Concurrent-Traffic-Simulation-cpp/build/CMakeFiles/traffic_simulation.dir/src/Vehicle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/TrafficLight.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <random>
+#include <future>
+#include "TrafficLight.h"
+
+/* Implementation of class "MessageQueue" */
+
+ 
+template <typename T>
+T MessageQueue<T>::receive()
+{
+    //The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
+    // to wait for and receive new messages and pull them from the queue using move semantics. 
+    // The received object should then be returned by the receive function. 
+    std::unique_lock<std::mutex> ulock(_qmtx);
+    _cond.wait(ulock, [this]{return !_queue.empty();});
+
+    T msg = std::move(_queue.back());
+    _queue.pop_back();
+    return msg;
+}
+
+template <typename T>
+void MessageQueue<T>::send(T &&msg)
+{
+    // The method send should use the mechanisms std::lock_guard<std::mutex> 
+    // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> lockGuard(_qmtx);
+    _queue.push_back(std::move(msg));
+    _cond.notify_one();
+}
+
+
+/* Implementation of class "TrafficLight" */
+ 
+TrafficLight::TrafficLight()
+{
+    _currentPhase = TrafficLightPhase::red;
+}
+
+void TrafficLight::waitForGreen()
+{
+    // add the implementation of the method waitForGreen, in which an infinite while-loop 
+    // runs and repeatedly calls the receive function on the message queue. 
+    // Once it receives TrafficLightPhase::green, the method returns.
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+        auto msg = _msg_queue.receive();
+        if (msg == TrafficLightPhase::green)
+        {
+            break;
+        }
+    }
+    return;
+}
+
+TrafficLightPhase TrafficLight::getCurrentPhase()
+{
+    return _currentPhase;
+}
+
+void TrafficLight::simulate()
+{
+    //Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
+}
+
+// virtual function which is executed in a thread
+void TrafficLight::cycleThroughPhases()
+{
+    // Implemented the function with an infinite loop that measures the time between two loop cycles 
+    // and toggles the current phase of the traffic light between red and green and sends an update method 
+    // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
+    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles.
+
+    std::unique_lock<std::mutex> uLock(_mtx);
+    uLock.unlock();
+
+	/* Init our random generation between 4 and 6 seconds */
+	std::random_device rd;
+	std::mt19937 eng(rd());
+	std::uniform_int_distribution<> distr(4000, 6000);
+
+    //cycle duration between traffic light toggle in ms
+    double cycle_duration = distr(eng); //Duration of a single simulation cycle in seconds, is randomly chosen
+    std::cout << "Traffic Light #" << _id << " Started " << cycle_duration  << std::endl;
+    std::chrono::time_point<std::chrono::system_clock> lastUpdate;
+    lastUpdate = std::chrono::system_clock::now();
+
+    while(true)
+    {
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        long timeSinceLastUpdate = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - lastUpdate).count();
+
+        if(timeSinceLastUpdate >= cycle_duration)
+        {
+
+            //Toggle the phase
+            if(_currentPhase == TrafficLightPhase::red)
+            {
+                _currentPhase = TrafficLightPhase::green;
+		std::cout << "Traffic Light #" << _id << "Green" << std::endl;
+            }
+            else
+            {
+                _currentPhase = TrafficLightPhase::red;
+		std::cout << "Traffic Light #" << _id << " Red" << std::endl;
+            }
+            
+            //send update to message queue
+            auto msg = _currentPhase;
+            std::future<void> updateMsgQueue = std::async(std::launch::async, &MessageQueue<TrafficLightPhase>::send, &_msg_queue, std::move(msg));
+            updateMsgQueue.wait();
+
+            //Reset the stop watch
+            lastUpdate = std::chrono::system_clock::now();
+
+            //Randomly choose the cycle duration for the next cycle
+	    cycle_duration = distr(eng);
+        }
+    }
+}
+
